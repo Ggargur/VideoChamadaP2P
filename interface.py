a3c67e805422621a8d024337e0bc2d95494b67c8
Eric Vianna <69487403+azhidahak@users.noreply.github.com>
2023-09-27 18:32:25 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,1 +1,31 @@
+import tkinter
+from tkinter import ttk
+from client import register_name, request_name
 
+
+class Interface(tkinter.Tk):
+    def __init__(self):
+        super().__init__()
+        self.create_interface()
+
+    def create_interface(self):
+        self.columnconfigure(0)
+
+        self.title("LOGIN")
+
+        ttk.Label(self, text="Nome de usuário: ").
+        ttk.Entry(self).
+
+        ttk.Button(self, text="Cadastrar", command=register_name).
+
+        ttk.Button(self, text="Requisitar", command=request_name).
+
+    def on_closing(self):
+        self.destroy()
+
+
+
+
+if __name__ == "__main__":
+    interface = Interface()
+    interface.mainloop()
